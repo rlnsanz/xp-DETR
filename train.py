71c2e9d1c12f9d9c1e7804551aa496e3d06555ff
Alexis Wan <60990214+alexiswan99@users.noreply.github.com>
2023-05-16 15:20:25 -0700

    add test for DETR model (#1)
    Co-authored-by: Alexis Wan <alexiswan@Alexiss-MacBook-Pro.local>

--- train.py
+++ train.py
@@ -12,6 +12,10 @@ from transformers import AutoImageProcessor, AutoModelForObjectDetection
 
 import flor
 from flor import MTK as Flor
+import json
+import torchvision
+import evaluate
+from tqdm import tqdm
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -96,7 +100,7 @@ def collate_fn(batch):
 
 
 batch_size = 8
-num_epochs = 10
+num_epochs = 0
 learning_rate = 1e-5
 weight_decay = 1e-4
 
@@ -106,6 +110,7 @@ train_loader = torchdata.DataLoader(
     shuffle=True,
     collate_fn=collate_fn,
 )
+
 optimizer = torch.optim.AdamW(
     model.parameters(), lr=learning_rate, weight_decay=weight_decay
 )
@@ -130,4 +135,108 @@ for epoch in Flor.loop(range(num_epochs)):
         if i > 9:
             break
 
+
 print("Model TEST")
+
+# format annotations the same as for training, no need for data augmentation
+def val_formatted_anns(image_id, objects):
+    annotations = []
+    for i in range(0, len(objects["id"])):
+        new_ann = {
+            "id": objects["id"][i],
+            "category_id": objects["category"][i],
+            "iscrowd": 0,
+            "image_id": image_id,
+            "area": objects["area"][i],
+            "bbox": objects["bbox"][i],
+        }
+        annotations.append(new_ann)
+
+    return annotations
+
+
+# Save images and annotations into the files torchvision.datasets.CocoDetection expects
+def save_cppe5_annotation_file_images(cppe5):
+    output_json = {}
+    path_output_cppe5 = f"{os.getcwd()}/cppe5/"
+
+    if not os.path.exists(path_output_cppe5):
+        os.makedirs(path_output_cppe5)
+
+    path_anno = os.path.join(path_output_cppe5, "cppe5_ann.json")
+    categories_json = [{"supercategory": "none", "id": id, "name": id2label[id]} for id in id2label]
+    output_json["images"] = []
+    output_json["annotations"] = []
+    for example in cppe5:
+        ann = val_formatted_anns(example["image_id"], example["objects"])
+        output_json["images"].append(
+            {
+                "id": example["image_id"],
+                "width": example["image"].width,
+                "height": example["image"].height,
+                "file_name": f"{example['image_id']}.png",
+            }
+        )
+        output_json["annotations"].extend(ann)
+    output_json["categories"] = categories_json
+
+    with open(path_anno, "w") as file:
+        json.dump(output_json, file, ensure_ascii=False, indent=4)
+
+    for im, img_id in zip(cppe5["image"], cppe5["image_id"]):
+        path_img = os.path.join(path_output_cppe5, f"{img_id}.png")
+        im.save(path_img)
+
+    return path_output_cppe5, path_anno
+
+
+class CocoDetection(torchvision.datasets.CocoDetection):
+    def __init__(self, img_folder, feature_extractor, ann_file):
+        super().__init__(img_folder, ann_file)
+        self.feature_extractor = feature_extractor
+
+    def __getitem__(self, idx):
+        # read in PIL image and target in COCO format
+        img, target = super(CocoDetection, self).__getitem__(idx)
+
+        # preprocess image and target: converting target to DETR format,
+        # resizing + normalization of both image and target)
+        image_id = self.ids[idx]
+        target = {"image_id": image_id, "annotations": target}
+        encoding = self.feature_extractor(images=img, annotations=target, return_tensors="pt")
+        pixel_values = encoding["pixel_values"].squeeze()  # remove batch dimension
+        target = encoding["labels"][0]  # remove batch dimension
+
+        return {"pixel_values": pixel_values, "labels": target}
+
+
+path_output_cppe5, path_anno = save_cppe5_annotation_file_images(cppe5["test"])
+test_ds_coco_format = CocoDetection(path_output_cppe5, image_processor, path_anno)
+
+module = evaluate.load("ybelkada/cocoevaluate", coco=test_ds_coco_format.coco)
+val_dataloader = torch.utils.data.DataLoader(
+    test_ds_coco_format, batch_size=8, shuffle=False, collate_fn=collate_fn
+)
+
+model.eval()
+with torch.no_grad():
+    for idx, batch in enumerate(tqdm(val_dataloader)):
+        pixel_values = batch["pixel_values"]
+        pixel_mask = batch["pixel_mask"]
+
+        labels = [
+            {k: v for k, v in t.items()} for t in batch["labels"]
+        ]  # these are in DETR format, resized + normalized
+
+        # forward pass
+        outputs = model(pixel_values=pixel_values, pixel_mask=pixel_mask)
+
+        orig_target_sizes = torch.stack([target["orig_size"] for target in labels], dim=0)
+        results = image_processor.post_process(outputs, orig_target_sizes)  # convert outputs of model to COCO api
+
+        module.add(prediction=results, reference=labels)
+        del batch
+        print("check")
+results = module.compute()
+print(results)
+
