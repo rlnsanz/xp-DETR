6c864808b93c3882b8246042e861152cfb6b4eb3
Rolando Garcia <rogarcia@berkeley.edu>
2023-07-13 17:29:39 +0000

    initializing

---
+++ train.py
@@ -0,0 +1,250 @@
+import albumentations
+import numpy as np
+import os
+from PIL import Image, ImageDraw
+
+import torch
+import torch.nn as nn
+from torch.utils import data as torchdata
+
+from datasets import load_dataset, DatasetDict
+from transformers import AutoImageProcessor, AutoModelForObjectDetection
+
+import flor
+from flor import MTK as Flor
+import json
+import torchvision
+import evaluate
+from tqdm import tqdm
+
+device = flor.arg("device", default=torch.device("cuda" if torch.cuda.is_available() else "cpu"))
+
+cppe5 = load_dataset("cppe-5")
+assert isinstance(cppe5, DatasetDict)
+categories = cppe5["train"].features["objects"].feature["category"].names  # type: ignore
+id2label = {index: x for index, x in enumerate(categories, start=0)}
+label2id = {v: k for k, v in id2label.items()}
+
+remove_idx = [590, 821, 822, 875, 876, 878, 879]
+keep = [i for i in range(len(cppe5["train"])) if i not in remove_idx]
+cppe5["train"] = cppe5["train"].select(keep)
+
+model_name = "facebook/detr-resnet-50"
+image_processor = AutoImageProcessor.from_pretrained(model_name)
+model = AutoModelForObjectDetection.from_pretrained(
+    model_name,
+    id2label=id2label,
+    label2id=label2id,
+    ignore_mismatched_sizes=True,
+).to(device)
+Flor.checkpoints(model)
+
+transform = albumentations.Compose(
+    [
+        albumentations.Resize(480, 480),
+        albumentations.HorizontalFlip(p=1.0),
+        albumentations.RandomBrightnessContrast(p=1.0),
+    ],
+    bbox_params=albumentations.BboxParams(format="coco", label_fields=["category"]),
+)
+
+
+def formatted_anns(image_id, category, area, bbox):
+    annotations = []
+    for i in range(0, len(category)):
+        new_ann = {
+            "image_id": image_id,
+            "category_id": category[i],
+            "isCrowd": 0,
+            "area": area[i],
+            "bbox": list(bbox[i]),
+        }
+        annotations.append(new_ann)
+
+    return annotations
+
+
+def transform_aug_ann(examples):
+    image_ids = examples["image_id"]
+    images, bboxes, area, categories = [], [], [], []
+    for image, objects in zip(examples["image"], examples["objects"]):
+        image = np.array(image.convert("RGB"))[:, :, ::-1]
+        out = transform(
+            image=image, bboxes=objects["bbox"], category=objects["category"]
+        )
+
+        area.append(objects["area"])
+        images.append(out["image"])
+        bboxes.append(out["bboxes"])
+        categories.append(out["category"])
+
+    targets = [
+        {"image_id": id_, "annotations": formatted_anns(id_, cat_, ar_, box_)}
+        for id_, cat_, ar_, box_ in zip(image_ids, categories, area, bboxes)
+    ]
+
+    return image_processor(images=images, annotations=targets, return_tensors="pt")
+
+
+def collate_fn(batch):
+    pixel_values = [item["pixel_values"] for item in batch]
+    encoding = image_processor.pad_and_create_pixel_mask(
+        pixel_values, return_tensors="pt"
+    )
+    labels = [item["labels"].to(device) for item in batch]
+    batch = {}
+    batch["pixel_values"] = encoding["pixel_values"].to(device)
+    batch["pixel_mask"] = encoding["pixel_mask"].to(device)
+    batch["labels"] = labels
+    return batch
+
+
+batch_size = flor.arg("batch_size", 8)
+num_epochs = flor.arg("epochs", default=5)
+learning_rate = flor.arg("learning_rate", 1e-5)
+weight_decay = flor.arg("weight_decay", 1e-4)
+
+train_loader = torchdata.DataLoader(
+    dataset=cppe5["train"].with_transform(transform_aug_ann),  # type: ignore
+    batch_size=batch_size,
+    shuffle=True,
+    collate_fn=collate_fn,
+)
+
+optimizer = torch.optim.AdamW(
+    model.parameters(), lr=learning_rate, weight_decay=weight_decay
+)
+Flor.checkpoints(optimizer)
+
+total_step = len(train_loader)
+for epoch in Flor.loop(range(num_epochs)):
+    model.train()
+    for i, batch in Flor.loop(enumerate(train_loader)):
+        outputs = model(**batch)
+        loss = outputs.loss
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        print(
+            "Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}".format(
+                epoch + 1, num_epochs, i, total_step, flor.log("loss", loss.item())
+            )
+        )
+
+
+print("Model TEST")
+
+
+# format annotations the same as for training, no need for data augmentation
+def val_formatted_anns(image_id, objects):
+    annotations = []
+    for i in range(0, len(objects["id"])):
+        new_ann = {
+            "id": objects["id"][i],
+            "category_id": objects["category"][i],
+            "iscrowd": 0,
+            "image_id": image_id,
+            "area": objects["area"][i],
+            "bbox": objects["bbox"][i],
+        }
+        annotations.append(new_ann)
+
+    return annotations
+
+
+# Save images and annotations into the files torchvision.datasets.CocoDetection expects
+def save_cppe5_annotation_file_images(cppe5):
+    output_json = {}
+    path_output_cppe5 = f"{os.getcwd()}/cppe5/"
+
+    if not os.path.exists(path_output_cppe5):
+        os.makedirs(path_output_cppe5)
+
+    path_anno = os.path.join(path_output_cppe5, "cppe5_ann.json")
+    categories_json = [
+        {"supercategory": "none", "id": id, "name": id2label[id]} for id in id2label
+    ]
+    output_json["images"] = []
+    output_json["annotations"] = []
+    for example in cppe5:
+        ann = val_formatted_anns(example["image_id"], example["objects"])
+        output_json["images"].append(
+            {
+                "id": example["image_id"],
+                "width": example["image"].width,
+                "height": example["image"].height,
+                "file_name": f"{example['image_id']}.png",
+            }
+        )
+        output_json["annotations"].extend(ann)
+    output_json["categories"] = categories_json
+
+    with open(path_anno, "w") as file:
+        json.dump(output_json, file, ensure_ascii=False, indent=4)
+
+    for im, img_id in zip(cppe5["image"], cppe5["image_id"]):
+        path_img = os.path.join(path_output_cppe5, f"{img_id}.png")
+        im.save(path_img)
+
+    return path_output_cppe5, path_anno
+
+
+class CocoDetection(torchvision.datasets.CocoDetection):
+    def __init__(self, img_folder, feature_extractor, ann_file):
+        super().__init__(img_folder, ann_file)
+        self.feature_extractor = feature_extractor
+
+    def __getitem__(self, idx):
+        # read in PIL image and target in COCO format
+        img, target = super(CocoDetection, self).__getitem__(idx)
+
+        # preprocess image and target: converting target to DETR format,
+        # resizing + normalization of both image and target)
+        image_id = self.ids[idx]
+        target = {"image_id": image_id, "annotations": target}
+        encoding = self.feature_extractor(
+            images=img, annotations=target, return_tensors="pt"
+        )
+        pixel_values = encoding["pixel_values"].squeeze()  # remove batch dimension
+        target = encoding["labels"][0]  # remove batch dimension
+
+        return {"pixel_values": pixel_values, "labels": target}
+
+
+path_output_cppe5, path_anno = save_cppe5_annotation_file_images(cppe5["test"])
+test_ds_coco_format = CocoDetection(path_output_cppe5, image_processor, path_anno)
+
+module = evaluate.load("ybelkada/cocoevaluate", coco=test_ds_coco_format.coco)
+val_dataloader = torch.utils.data.DataLoader(  # type: ignore
+    test_ds_coco_format, batch_size=8, shuffle=False, collate_fn=collate_fn
+)
+
+model.eval()
+with torch.no_grad():
+    for idx, batch in enumerate(tqdm(val_dataloader)):
+        if idx >= 1000:
+            break
+        pixel_values = batch["pixel_values"]
+        pixel_mask = batch["pixel_mask"]
+
+        labels = [
+            {k: v for k, v in t.items()} for t in batch["labels"]
+        ]  # these are in DETR format, resized + normalized
+
+        # forward pass
+        outputs = model(pixel_values=pixel_values, pixel_mask=pixel_mask)
+
+        orig_target_sizes = torch.stack(
+            [target["orig_size"] for target in labels], dim=0
+        )
+        results = image_processor.post_process(
+            outputs, orig_target_sizes
+        )  # convert outputs of model to COCO api
+
+        module.add(prediction=results, reference=labels)
+        del batch
+        print("check", idx)
+results = module.compute()
+print(results)
